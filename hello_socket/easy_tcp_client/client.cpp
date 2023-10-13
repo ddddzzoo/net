598468da3034c1e1a7d3b473c429072ea22effbc
dddd <873720581@qq.com>
2023-10-13 19:54:15 +0800

    windows平台适配

--- hello_socket/easy_tcp_client/client.cpp
+++ hello_socket/easy_tcp_client/client.cpp
@@ -1,185 +1,235 @@
+#ifdef _WIN32
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+#include <Ws2tcpip.h>
+#include <windows.h>
+#include <winsock2.h>
+#pragma comment(lib, "Ws2_32.lib")
+#else
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#define SOCKET int
+#define INVALID_SOCKET (SOCKET)(~0)
+#define SOCKET_ERROR (-1)
+#endif  // _WIN32
+
 #include <iostream>
 #include <string>
 #include <thread>
-#include <unistd.h>
-#include <arpa/inet.h>
-#include <sys/socket.h>
 
-const char *SERVER_IP = "127.0.0.1";
+const char* SERVER_IP = "127.0.0.1";
 const int SERVER_PORT = 12345;
 
 // 使用结构体模拟用户登陆请求
 // 头部
 enum CMD {
-    CMD_LOGIN,
-    CMD_LOGIN_RESULT,
-    CMD_LOGOUT,
-    CMD_LOGOUT_RESULT,
-    CMD_NEW_USER_JOIN,
-    CMD_ERROR
+  CMD_LOGIN,
+  CMD_LOGIN_RESULT,
+  CMD_LOGOUT,
+  CMD_LOGOUT_RESULT,
+  CMD_NEW_USER_JOIN,
+  CMD_ERROR
 };
 
 struct DataHeader {
-    short data_length; // 数据长度
-    short cmd;  // 数据作用
+  short data_length;  // 数据长度
+  short cmd;          // 数据作用
 };
 
 // 数据部分
 struct Login : public DataHeader {
-    Login() : DataHeader() {
-        data_length = sizeof(Login);
-        cmd = CMD_LOGIN;
-    }
+  Login() : DataHeader() {
+    data_length = sizeof(Login);
+    cmd = CMD_LOGIN;
+  }
 
-    std::string username;
-    std::string password;
+  std::string username;
+  std::string password;
 };
 
 struct LoginResult : public DataHeader {
-    LoginResult() : DataHeader(), result(0) {
-        data_length = sizeof(LoginResult);
-        cmd = CMD_LOGIN_RESULT;
-    }
+  LoginResult() : DataHeader(), result(0) {
+    data_length = sizeof(LoginResult);
+    cmd = CMD_LOGIN_RESULT;
+  }
 
-    int result;
+  int result;
 };
 
 struct Logout : public DataHeader {
-    Logout() : DataHeader() {
-        data_length = sizeof(Logout);
-        cmd = CMD_LOGOUT;
-    }
+  Logout() : DataHeader() {
+    data_length = sizeof(Logout);
+    cmd = CMD_LOGOUT;
+  }
 
-    std::string username;
+  std::string username;
 };
 
 struct LogoutResult : public DataHeader {
-    LogoutResult() : DataHeader(), result(0) {
-        data_length = sizeof(LoginResult);
-        cmd = CMD_LOGOUT_RESULT;
-    }
+  LogoutResult() : DataHeader(), result(0) {
+    data_length = sizeof(LoginResult);
+    cmd = CMD_LOGOUT_RESULT;
+  }
 
-    int result;
+  int result;
 };
 
 struct NewUserJoin : public DataHeader {
-    NewUserJoin() : DataHeader(), socket(0) {
-        data_length = sizeof(NewUserJoin);
-        cmd = CMD_NEW_USER_JOIN;
-    }
+  NewUserJoin() : DataHeader(), socket(0) {
+    data_length = sizeof(NewUserJoin);
+    cmd = CMD_NEW_USER_JOIN;
+  }
 
-    int socket;
+  int socket;
 };
 
-int processor(int clientSocket) {
-    // 缓冲区
-    char buffer[4096] = {};
+int processor(int client_socket) {
+  // 缓冲区
+  char buffer[4096] = {};
 
-    int byte_len = static_cast<int>( recv(clientSocket, buffer, sizeof(DataHeader), 0));
-    auto *header = reinterpret_cast<DataHeader *>(buffer);
+  int byte_len =
+      static_cast<int>(recv(client_socket, buffer, sizeof(DataHeader), 0));
+  auto* header = reinterpret_cast<DataHeader*>(buffer);
 
-    if (byte_len <= 0) {
-        std::cout << "Connection lost!" << std::endl;
-        return -1;
-    }
+  if (byte_len <= 0) {
+    std::cout << "Connection lost!" << std::endl;
+    return -1;
+  }
 
-    switch (header->cmd) {
-        case CMD_LOGIN_RESULT: {
-            recv(clientSocket, buffer + sizeof(DataHeader), header->data_length - sizeof(DataHeader), 0);
-            auto login_result = reinterpret_cast<LoginResult *>(buffer);
-            std::cout << "CMD_LOGIN_RESULT -- Data length: " << login_result->data_length;
-        }
-            break;
-        case CMD_LOGOUT_RESULT: {
-            recv(clientSocket, buffer + sizeof(DataHeader), header->data_length - sizeof(DataHeader), 0);
-            auto logout_result = reinterpret_cast<LogoutResult *>(buffer);
-            std::cout << "CMD_LOGOUT_RESULT -- Data length: " << logout_result->data_length;
-        }
-            break;
-        case CMD_NEW_USER_JOIN: {
-            recv(clientSocket, buffer + sizeof(DataHeader), header->data_length - sizeof(DataHeader), 0);
-            auto new_user_join = reinterpret_cast<NewUserJoin *>(buffer);
-            std::cout << "CMD_NEW_USER_JOIN -- Data length: " << new_user_join->data_length;
-        }
-            break;
-    }
-    return 0;
+  switch (header->cmd) {
+    case CMD_LOGIN_RESULT: {
+      recv(client_socket, buffer + sizeof(DataHeader),
+           header->data_length - sizeof(DataHeader), 0);
+      auto login_result = reinterpret_cast<LoginResult*>(buffer);
+      std::cout << "CMD_LOGIN_RESULT -- Data length: "
+                << login_result->data_length;
+    } break;
+    case CMD_LOGOUT_RESULT: {
+      recv(client_socket, buffer + sizeof(DataHeader),
+           header->data_length - sizeof(DataHeader), 0);
+      auto logout_result = reinterpret_cast<LogoutResult*>(buffer);
+      std::cout << "CMD_LOGOUT_RESULT -- Data length: "
+                << logout_result->data_length;
+    } break;
+    case CMD_NEW_USER_JOIN: {
+      recv(client_socket, buffer + sizeof(DataHeader),
+           header->data_length - sizeof(DataHeader), 0);
+      auto new_user_join = reinterpret_cast<NewUserJoin*>(buffer);
+      std::cout << "CMD_NEW_USER_JOIN -- Data length: "
+                << new_user_join->data_length;
+    } break;
+  }
+  return 0;
 }
 
 bool run = true;
 
 void cmd_thread(int client_socket) {
-    while (true) {
-        std::string cmd;
-        std::cin >> cmd;
-        if ("exit" == cmd) {
-            run = false;
-            std::cout << "Exit cmd thread!" << std::endl;
-            break;
-        } else if ("login" == cmd) {
-            Login login;
-            login.username = "username";
-            login.password = "password";
-            // 发送消息给服务器
-            send(client_socket, (const char *) &login, sizeof(login), 0);
-        } else if ("logout" == cmd) {
-            Logout logout;
-            logout.username = "username";
-            // 发送消息给服务器
-            send(client_socket, (const char *) &logout, sizeof(logout), 0);
-        } else {
-            std::cout << "Unknown cmd!" << std::endl;
-        }
+  while (true) {
+    std::string cmd;
+    std::cin >> cmd;
+    if ("exit" == cmd) {
+      run = false;
+      std::cout << "Exit cmd thread!" << std::endl;
+      break;
     }
+    else if ("login" == cmd) {
+      Login login;
+      login.username = "username";
+      login.password = "password";
+      // 发送消息给服务器
+      send(client_socket, (const char*)&login, sizeof(login), 0);
+    }
+    else if ("logout" == cmd) {
+      Logout logout;
+      logout.username = "username";
+      // 发送消息给服务器
+      send(client_socket, (const char*)&logout, sizeof(logout), 0);
+    }
+    else {
+      std::cout << "Unknown cmd!" << std::endl;
+    }
+  }
 }
 
 int main() {
-    // 创建客户端 socket
-    int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
-    if (clientSocket == -1) {
-        std::cerr << "Creating socket failed!" << std::endl;
-        return 1;
+#ifdef _WIN32
+  // 初始化 Windows 套接字 DLL
+  WSADATA wsa_data;
+  if (WSAStartup(MAKEWORD(2, 2), &wsa_data) != 0) {
+    std::cout << "WSAStartup failed!" << std::endl;
+    return 1;
+  }
+#endif  // _WIN32
+
+  // 创建客户端 socket
+  SOCKET client_socket = socket(AF_INET, SOCK_STREAM, 0);
+  if (INVALID_SOCKET == client_socket) {
+    std::cerr << "Creating socket failed!" << std::endl;
+#ifdef _WIN32
+    WSACleanup();
+#endif  // _WIN32
+    return 1;
+  }
+
+  // 配置服务器地址
+  sockaddr_in server_addr = {};
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_port = htons(SERVER_PORT);
+#ifdef _WIN32
+  InetPton(AF_INET, SERVER_IP, &server_addr.sin_addr);
+#else
+  inetPton(AF_INET, SERVER_IP, &server_addr.sin_addr);
+#endif  // _WIN32
+
+  // 连接服务器
+  if (connect(client_socket, (sockaddr*)&server_addr, sizeof(server_addr)) ==
+      -1) {
+    std::cerr << "Connecting socket failed!" << std::endl;
+
+#ifdef _WIN32
+    closesocket(client_socket);
+    WSACleanup();
+#else
+    close(client_socket);
+#endif  // _WIN32
+    return 1;
+  }
+
+  std::thread t1(cmd_thread, client_socket);
+  t1.detach();
+
+  while (run) {
+    fd_set fd_read{};
+    FD_ZERO(&fd_read);
+    FD_SET(client_socket, &fd_read);
+    timeval time{1, 0};
+    int ret = select(client_socket + 1, &fd_read, nullptr, nullptr, &time);
+
+    if (ret < 0) {
+      std::cout << "Select over!" << std::endl;
+      break;
     }
 
-    // 配置服务器地址
-    sockaddr_in serverAddr = {};
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_port = htons(SERVER_PORT);
-    inet_pton(AF_INET, SERVER_IP, &(serverAddr.sin_addr));
+    if (FD_ISSET(client_socket, &fd_read)) {
+      FD_CLR(client_socket, &fd_read);
 
-    // 连接服务器
-    if (connect(clientSocket, (sockaddr *) &serverAddr, sizeof(serverAddr)) == -1) {
-        std::cerr << "Connecting socket failed!" << std::endl;
-        close(clientSocket);
-        return 1;
+      if (-1 == processor(client_socket)) {
+        std::cout << "Select over!!" << std::endl;
+        break;
+      }
     }
+  }
 
-    std::thread t1(cmd_thread, clientSocket);
-    t1.detach();
+  // 关闭客户端 socket
+#ifdef _WIN32
+  closesocket(client_socket);
+  WSACleanup();
+#else
+  close(client_socket);
+#endif  // _WIN32
 
-    while (run) {
-        fd_set fd_read;
-        FD_ZERO(&fd_read);
-        FD_SET(clientSocket, &fd_read);
-        timeval time{1, 0};
-        int ret = select(clientSocket + 1, &fd_read, nullptr, nullptr, &time);
-
-        if (ret < 0) {
-            std::cout << "Select over!" << std::endl;
-            break;
-        }
-
-        if (FD_ISSET(clientSocket, &fd_read)) {
-            FD_CLR(clientSocket, &fd_read);
-
-            if (-1 == processor(clientSocket)) {
-                std::cout << "Select over!!" << std::endl;
-                break;
-            }
-        }
-    }
-    // 关闭客户端 socket
-    close(clientSocket);
-
-    return 0;
+  return 0;
 }
--- hello_socket/easy_tcp_server/server.cpp
+++ hello_socket/easy_tcp_server/server.cpp
@@ -1,214 +1,282 @@
+﻿#ifdef _WIN32
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+#include <windows.h>
+#include <winsock2.h>
+#pragma comment(lib, "Ws2_32.lib")
+#pragma execution_character_set("utf-8")
+#else
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#define SOCKET int
+#define INVALID_SOCKET (SOCKET)(~0)
+#define SOCKET_ERROR (-1)
+#endif  // _WIN32
+
 #include <iostream>
 #include <string>
 #include <vector>
-#include <unistd.h>
-#include <arpa/inet.h>
-#include <sys/socket.h>
 
 const int PORT = 12345;
-const int BACKLOG = 5; // 等待队列的最大长度 超过会被服务器拒绝连接
+const int BACKLOG = 5;  // 等待队列的最大长度 超过会被服务器拒绝连接
 
 // 使用结构体模拟用户登陆请求
 // 头部
 enum CMD {
-    CMD_LOGIN,
-    CMD_LOGIN_RESULT,
-    CMD_LOGOUT,
-    CMD_LOGOUT_RESULT,
-    CMD_NEW_USER_JOIN,
-    CMD_ERROR
+  CMD_LOGIN,
+  CMD_LOGIN_RESULT,
+  CMD_LOGOUT,
+  CMD_LOGOUT_RESULT,
+  CMD_NEW_USER_JOIN,
+  CMD_ERROR
 };
 
 struct DataHeader {
-    short data_length; // 数据长度
-    short cmd;  // 数据作用
+  short data_length;  // 数据长度
+  short cmd;          // 数据作用
 };
 
 // 数据部分
 struct Login : public DataHeader {
-    Login() : DataHeader() {
-        data_length = sizeof(Login);
-        cmd = CMD_LOGIN;
-    }
+  Login() : DataHeader() {
+    data_length = sizeof(Login);
+    cmd = CMD_LOGIN;
+  }
 
-    std::string username;
-    std::string password;
+  std::string username;
+  std::string password;
 };
 
 struct LoginResult : public DataHeader {
-    LoginResult() : DataHeader(), result(0) {
-        data_length = sizeof(LoginResult);
-        cmd = CMD_LOGIN_RESULT;
-    }
+  LoginResult() : DataHeader(), result(0) {
+    data_length = sizeof(LoginResult);
+    cmd = CMD_LOGIN_RESULT;
+  }
 
-    int result;
+  int result;
 };
 
 struct Logout : public DataHeader {
-    Logout() : DataHeader() {
-        data_length = sizeof(Logout);
-        cmd = CMD_LOGOUT;
-    }
+  Logout() : DataHeader() {
+    data_length = sizeof(Logout);
+    cmd = CMD_LOGOUT;
+  }
 
-    std::string username;
+  std::string username;
 };
 
 struct LogoutResult : public DataHeader {
-    LogoutResult() : DataHeader(), result(0) {
-        data_length = sizeof(LoginResult);
-        cmd = CMD_LOGOUT_RESULT;
-    }
+  LogoutResult() : DataHeader(), result(0) {
+    data_length = sizeof(LoginResult);
+    cmd = CMD_LOGOUT_RESULT;
+  }
 
-    int result;
+  int result;
 };
 
 struct NewUserJoin : public DataHeader {
-    NewUserJoin() : DataHeader(), socket(0) {
-        data_length = sizeof(NewUserJoin);
-        cmd = CMD_NEW_USER_JOIN;
-    }
+  NewUserJoin() : DataHeader(), socket(0) {
+    data_length = sizeof(NewUserJoin);
+    cmd = CMD_NEW_USER_JOIN;
+  }
 
-    int socket;
+  int socket;
 };
 
-int processor(int clientSocket) {
-    // 缓冲区
-    char buffer[4096] = {};
-    // 接受客户端消息
-    int byte_len = static_cast<int>( recv(clientSocket, buffer, sizeof(DataHeader), 0));
-    auto *header = reinterpret_cast<DataHeader *>(buffer);
+int processor(int client_socket) {
+  // 缓冲区
+  char buffer[4096] = {};
+  // 接受客户端消息
+  int byte_len =
+      static_cast<int>(recv(client_socket, buffer, sizeof(DataHeader), 0));
+  auto *header = reinterpret_cast<DataHeader *>(buffer);
 
-    if (byte_len <= 0) {
-        std::cout << "Client exit!" << std::endl;
-        return -1;
-    }
+  if (byte_len <= 0) {
+    std::cout << "Client exit!" << std::endl;
+    return -1;
+  }
 
-    switch (header->cmd) {
-        case CMD_LOGIN: {
-            recv(clientSocket, buffer + sizeof(DataHeader), header->data_length - sizeof(DataHeader), 0);
-            auto login = reinterpret_cast<Login *>(buffer);
-            std::cout << "Login -- " << "Socket: " << clientSocket << ", data length: " << login->data_length
-                      << ", username: " << login->username << ", password: " << login->password << std::endl;
-            // 没有判断用户密码
-            LoginResult ret;
-            send(clientSocket, (char *) &ret, sizeof(LoginResult), 0);
-        }
-            break;
-        case CMD_LOGOUT: {
-            recv(clientSocket, buffer + sizeof(DataHeader), header->data_length - sizeof(DataHeader), 0);
-            auto logout = reinterpret_cast<Logout *>(buffer);
-            std::cout << "Logout -- " << "Socket: " << clientSocket << ", data length: " << logout->data_length
-                      << ", username: " << logout->username << std::endl;
-            LogoutResult ret;
-            send(clientSocket, (char *) &ret, sizeof(ret), 0);
-        }
-            break;
-        default:
-            header->cmd = CMD_ERROR;
-            header->data_length = 0;
-            send(clientSocket, (char *) &header, sizeof(*header), 0);
-            break;
-    }
-    return 0;
+  switch (header->cmd) {
+    case CMD_LOGIN: {
+      recv(client_socket, buffer + sizeof(DataHeader),
+           header->data_length - sizeof(DataHeader), 0);
+      auto login = reinterpret_cast<Login *>(buffer);
+      std::cout << "Login -- "
+                << "Socket: " << client_socket
+                << ", data length: " << login->data_length
+                << ", username: " << login->username
+                << ", password: " << login->password << std::endl;
+      // 没有判断用户密码
+      LoginResult ret;
+      send(client_socket, (char *)&ret, sizeof(LoginResult), 0);
+    } break;
+    case CMD_LOGOUT: {
+      recv(client_socket, buffer + sizeof(DataHeader),
+           header->data_length - sizeof(DataHeader), 0);
+      auto logout = reinterpret_cast<Logout *>(buffer);
+      std::cout << "Logout -- "
+                << "Socket: " << client_socket
+                << ", data length: " << logout->data_length
+                << ", username: " << logout->username << std::endl;
+      LogoutResult ret;
+      send(client_socket, (char *)&ret, sizeof(ret), 0);
+    } break;
+    default:
+      header->cmd = CMD_ERROR;
+      header->data_length = 0;
+      send(client_socket, (char *)&header, sizeof(*header), 0);
+      break;
+  }
+  return 0;
 }
 
 int main() {
-    // 客户端套接字集合
-    std::vector<int> clients;
-    // 创建服务器套接字
-    int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
-    if (serverSocket == -1) {
-        std::cerr << "Creating socket failed!" << std::endl;
-        return 1;
+#ifdef _WIN32
+  // 初始化 Windows 套接字 DLL
+  WSADATA wsa_data;
+  if (WSAStartup(MAKEWORD(2, 2), &wsa_data) != 0) {
+    std::cout << "WSAStartup failed!" << std::endl;
+    return 1;
+  }
+#endif  // _WIN32
+
+  // 客户端套接字集合
+  std::vector<SOCKET> clients;
+  // 创建服务器套接字
+  SOCKET server_socket = socket(AF_INET, SOCK_STREAM, 0);
+  if (INVALID_SOCKET == server_socket) {
+    std::cerr << "Creating socket failed!" << std::endl;
+#ifdef _WIN32
+    WSACleanup();
+#endif  // _WIN32
+    return 1;
+  }
+
+  // 配置服务器地址
+  sockaddr_in server_addr = {};
+  server_addr.sin_family = AF_INET;    // 使用IPV4
+  server_addr.sin_port = htons(PORT);  // host to network short
+
+#ifdef _WIN32
+  server_addr.sin_addr.S_un.S_addr = INADDR_ANY;
+#else
+  server_addr.sin_addr.s_addr =
+      INADDR_ANY;  // 设置为相应的IP地址 INADDR_ANY 接受来自任何IP的请求
+#endif  // _WIN32
+
+  // 绑定服务器 socket
+  if (SOCKET_ERROR ==
+      bind(server_socket, (sockaddr *)&server_addr, sizeof(server_addr))) {
+    std::cerr << "Binding socket failed!" << std::endl;
+#ifdef _WIN32
+    WSACleanup();
+#endif  // _WIN32
+    return 1;
+  }
+
+  // 监听请求
+  if (SOCKET_ERROR == listen(server_socket, BACKLOG)) {
+    std::cerr << "Listening socket failed!" << std::endl;
+#ifdef _WIN32
+    closesocket(server_socket);
+    WSACleanup();
+#else
+    close(server_socket);
+#endif  // _WIN32
+    return 1;
+  }
+
+  std::cout << "Server is listening on port " << PORT << std::endl;
+
+  while (true) {
+    // 描述符（socket）集合
+    fd_set fd_read{};   // 可读状态文件描述符
+    fd_set fd_write{};  // 可写状态文件描述符
+    fd_set fd_exp{};    // 异常状态文件描述符
+    // 清理集合
+    FD_ZERO(&fd_read);
+    FD_ZERO(&fd_write);
+    FD_ZERO(&fd_exp);
+    // 将描述符加入集合
+    FD_SET(server_socket, &fd_read);
+    FD_SET(server_socket, &fd_write);
+    FD_SET(server_socket, &fd_exp);
+
+    SOCKET maxfd = server_socket;
+    for (int i = static_cast<int>(clients.size()) - 1; i >= 0; --i) {
+      FD_SET(clients.at(i), &fd_read);
+      if (maxfd < clients.at(i)) {
+        maxfd = clients.at(i);
+      }
     }
 
-    // 配置服务器地址
-    sockaddr_in serverAddr = {};
-    serverAddr.sin_family = AF_INET; // 使用IPV4
-    serverAddr.sin_port = htons(PORT); // host to network short
-    serverAddr.sin_addr.s_addr = INADDR_ANY; // 设置为相应的IP地址 INADDR_ANY 接受来自任何IP的请求
-
-    // 绑定服务器 socket
-    if (bind(serverSocket, (sockaddr *) &serverAddr, sizeof(serverAddr)) == -1) {
-        std::cerr << "Binding socket failed!" << std::endl;
-        return 1;
+    timeval time = {1, 0};
+    int ret = select(maxfd + 1, &fd_read, &fd_write, &fd_exp, &time);
+    if (ret < 0) {
+      std::cout << "Select over!" << std::endl;
+      break;
     }
 
-    // 监听请求
-    if (listen(serverSocket, BACKLOG) == -1) {
-        std::cerr << "Listening socket failed!" << std::endl;
-        close(serverSocket);
-        return 1;
-    }
+    if (FD_ISSET(server_socket, &fd_read)) {
+      FD_CLR(server_socket, &fd_read);
+      // 接受客户端
+      sockaddr_in client_addr = {};
+      int client_addr_len = sizeof(client_addr);
+      SOCKET client_socket = INVALID_SOCKET;
 
-    std::cout << "Server is listening on port " << PORT << std::endl;
+#ifdef _WIN32
+      client_socket =
+          accept(server_socket, (sockaddr *)&client_socket, &client_addr_len);
+#else
+      client_socket = accept(server_socket, (sockaddr *)&client_addr,
+                             (socklen_t *)&client_addr_len);
+#endif  // _WIN32
 
-    while (true) {
-        // 描述符（socket）集合
-        fd_set fd_read; // 可读状态文件描述符
-        fd_set fd_write; // 可写状态文件描述符
-        fd_set fd_exp; // 异常状态文件描述符
-        // 清理集合
-        FD_ZERO(&fd_read);
-        FD_ZERO(&fd_write);
-        FD_ZERO(&fd_exp);
-        // 将描述符加入集合
-        FD_SET(serverSocket, &fd_read);
-        FD_SET(serverSocket, &fd_write);
-        FD_SET(serverSocket, &fd_exp);
-
-        int maxfd = serverSocket;
-        for (int i = static_cast<int>( clients.size() - 1); i >= 0; --i) {
-            FD_SET(clients.at(i), &fd_read);
-            if (maxfd < clients.at(i)) {
-                maxfd = clients.at(i);
-            }
-        }
-
-        timeval time = {1, 0};
-        int ret = select(maxfd + 1, &fd_read, &fd_write, &fd_exp, &time);
-        if (ret < 0) {
-            std::cout << "Select over!" << std::endl;
-            break;
-        }
-
-        if (FD_ISSET(serverSocket, &fd_read)) {
-            FD_CLR(serverSocket, &fd_read);
-            // 接受客户端
-            sockaddr_in clientAddr = {};
-            socklen_t clientAddrLen = sizeof(clientAddr);
-            int clientSocket = accept(serverSocket, (sockaddr *) &clientAddr, &clientAddrLen);
-            if (clientSocket == -1) {
-                std::cerr << "Accepting socket failed!" << std::endl;
-            } else {
-                for (int i = static_cast<int>(clients.size()) - 1; i >= 0; --i) {
-                    NewUserJoin new_user_join;
-                    new_user_join.socket = clientSocket;
-                    send(clients.at(i), (const char *) &new_user_join, sizeof(NewUserJoin), 0);
-                }
-                clients.emplace_back(clientSocket);
-                std::cout << "New client：socket = " << clientSocket << ", IP = "
-                          << inet_ntoa(clientAddr.sin_addr) << std::endl;
-            }
-        }
-
+      if (INVALID_SOCKET == client_socket) {
+        std::cerr << "Accepting socket failed!" << std::endl;
+      }
+      else {
         for (int i = static_cast<int>(clients.size()) - 1; i >= 0; --i) {
-            if (FD_ISSET(clients.at(i), &fd_read)) {
-                if (-1 == processor(clients.at(i))) {
-                    auto iter = clients.begin() + i;
-                    if (iter != clients.end()) {
-                        clients.erase(iter);
-                    }
-                }
-            }
+          NewUserJoin new_user_join;
+          new_user_join.socket = client_socket;
+          send(clients.at(i), (const char *)&new_user_join, sizeof(NewUserJoin),
+               0);
         }
+        clients.emplace_back(client_socket);
+        std::cout << "New client：socket = " << (int)client_socket
+                  << ", IP = " << inet_ntoa(client_addr.sin_addr) << std::endl;
+      }
     }
 
     for (int i = static_cast<int>(clients.size()) - 1; i >= 0; --i) {
-        close(clients.at(i));
+      if (FD_ISSET(clients.at(i), &fd_read)) {
+        if (-1 == processor(clients.at(i))) {
+          auto iter = clients.begin() + i;
+          if (iter != clients.end()) {
+            clients.erase(iter);
+          }
+        }
+      }
     }
+  }
 
-    // 关闭服务器 socket
-    close(serverSocket);
-    return 0;
+  for (int i = static_cast<int>(clients.size()) - 1; i >= 0; --i) {
+#ifdef _WIN32
+    closesocket(clients.at(i));
+#else
+    close(clients.at(i));
+#endif  // _WIN32
+  }
+
+  // 关闭服务器 socket
+#ifdef _WIN32
+  closesocket(server_socket);
+  WSACleanup();
+#else
+  close(server_socket);
+#endif  // _WIN32
+
+  return 0;
 }
